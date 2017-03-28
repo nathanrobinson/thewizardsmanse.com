335fd5a10d3ba56be92a2dd6fef37b0d684003cf
nathanrobinson <nathanrobinson@yahoo.com>
2017-03-27 19:57:13 -0500

    add npm build config

--- public/ads.js
+++
@@ -1,1 +0,0 @@
-var Running=10;
--- public/script.js
+++
@@ -1,7 +0,0 @@
-if (Running != 10) {
-    var message = "<p>This site uses non-intrusive advertisements to fund free content, please consider disabling your ad blocking software or whitelisting www.thewizardsmanse.com.</p>";
-    var list = document.getElementsByTagName("article");
-    for(var c=0; c<list.length; c++) {
-        list[c].innerHTML = message;
-    }
-}
