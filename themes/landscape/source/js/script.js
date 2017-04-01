7bd1908c0d78bf654350e4ae07d609a920c2d42b
Nathan Robinson <nathanrobinson@yahoo.com>
2017-04-01 16:39:23 -0500

    ad-block detector

--- themes/landscape/source/js/script.js
+++ themes/landscape/source/js/script.js
@@ -1,4 +1,11 @@
 (function($){
+  if (Running != 10) {
+    var message = "<p>This site uses non-intrusive advertisements to fund free content, please consider disabling your ad blocking software or whitelisting www.thewizardsmanse.com.</p>";
+    var list = document.getElementsByTagName("article");
+    for(var c=0; c<list.length; c++) {
+      list[c].innerHTML = message;
+    }
+  }
   // Search
   var $searchWrap = $('#search-form-wrap'),
     isSearchAnim = false,
